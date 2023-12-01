7a3c6c24c2d8ae4a7ad90a2e1b5e1488c6495358
Vasiloiu Ioana <96618238+vioana@users.noreply.github.com>
2023-12-01 19:58:04 +0100

    Update Proiect_POO.cpp

--- Proiect_POO.cpp
+++ Proiect_POO.cpp
@@ -47,86 +47,11 @@ int main() {
 				EquationSolver::solveQuadraticEquation(a, b, cVal);
 			}
 
-			cin.ignore(); // Ignore the newline character in the input buffer
+			cin.ignore(); 
 		}
 		else {
 			cout << "Invalid option. Please choose 1, 2, or 3." << endl;
 		}
 	}
-
-	/*EquationSolver eq1(2, 3, 4);
-	EquationSolver eq2(1, 2, 1);
-
-	EquationSolver sum = eq1 + eq2;
-	EquationSolver difference = eq1 - eq2;
-
-	cout << "Sum: " << sum << endl;
-	cout << "Difference: " << difference << endl;
-
-	Stack stack1, stack2;
-
-	stack1.push(1.1);
-	stack1.push(2.2);
-	stack1.push(3.3);
-
-	stack2.push(4.4);
-	stack2.push(5.5);
-	stack2.push(6.6);
-
-	// Use the overloaded + operator to add the two stacks
-	Stack sumStack = stack1 + stack2;
-
-	cout << "Stack 1:" << endl << stack1;
-	cout << "Stack 2:" << endl << stack2;
-	cout << "Sum of Stack 1 and Stack 2:" << endl << sumStack;
-
-	// Use the overloaded = operator to assign one stack to another
-	Stack assignedStack;
-	assignedStack = stack1;
-
-	cout << "Original Stack 1:" << endl << stack1;
-	cout << "Assigned Stack:" << endl << assignedStack;
-
-
-	// Test operator() and operator string()
-	Shunting_Yard shuntingYard;
-	shuntingYard("3+5*(2+6)");
-	Shunting_Yard sy2("3+5*(2+6)");
-
-	// Print the original expression and the result
-	cout << "Original Expression: " << shuntingYard.getExpresie() << endl;
-
-	if (shuntingYard == sy2) {
-		cout << "The expressions are equal." << endl;
-	} else {
-		cout << "The expressions are not equal." << endl;
-	}
-
-
-	Calculator calc;
-
-	calc.setExpresie("3+5*(2-6)");
-
-	calc.calculeaza();
-
-	// Check the ! operator
-	if (!calc) {
-		cout << "The result is empty." << endl;
-	} else {
-		cout << "The result is not empty." << endl;
-	}
-
-	// Check the [] operator for characters
-	char charAtIndex = calc[0];
-	if (charAtIndex != '\0') {
-		cout << "Character at index 0: " << charAtIndex << endl;
-	} else {
-		cout << "Invalid index or result is empty." << endl;
-	}
-
-
-	cout << "Original Expression: " << calc.getExpresie() << endl;
-	cout << "Result: " << calc.evalRPN(calc.getRezultat()) << endl;*/
-
 	return 0;
 }
